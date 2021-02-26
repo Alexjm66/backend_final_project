46ad627df7d92af18251fe82638248d75549cbf1
Alexander <alexjmaart665@gmail.com>
2021-02-26 11:17:24 +0200

    Updated

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def init_sqlite_db():
     con.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, email TEXT, username TEXT, password TEXT)')
     print("Table created successfully")
 
+
 init_sqlite_db()
 
 app = Flask (__name__)
@@ -25,25 +26,35 @@ def dict_factory(cursor, row):
 @app.route('/add_new_user/', methods=['POST'])
 def add_new_user():
      if request.method == "POST":
-         response = {}
-         response['msg']= None
+         msg= None
          try:
-            name = request.form['name']
-            surname = request.form['surname']
-            email = request.form['email']
-            username = request.form['username']
-            password = request.form['password']
+            post_data = request.get_json()
+            name = post_data['name']
+            surname = post_data['surname']
+            email = post_data['email']
+            username = post_data['username']
+            password = post_data['password']
 
             with sqlite3.connect('database.db') as conn:
                 cur = conn.cursor()
                 conn.row_factory = dict_factory
                 cur.execute("INSERT INTO user(name, surname, email, username, password) VALUES(?,?,?,?,?)", (name, surname, email, username, password))
                 conn.commit()
-                response['msg'] = "Record added successfully"
+                msg = "Record added successfully"
 
          except Exception as e:
-            conn.rollback()
-            response['msg'] = "Error occurred in insert operation: " + str(e)
+            msg = 'error'+ str(e)
 
          finally:
-            return response
+            return {'msg': msg}
+# @app.route('/list-users/', methods=['GET'])
+# # def list():
+# #     try:
+# #         with sqlite3.connect('database.db') as conn:
+# #                 cur = conn.cursor()
+# #                 conn.row_factory = dict_factory
+# #                 cur.execute("SELECT * FROM users")
+# #                 print(cur.fetchall())
+# #     except Exception as e:
+# #         conn.rollback()
+# #         response
